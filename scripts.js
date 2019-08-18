130b329e9b90d4ca7838b1013c9d09fa3b164cb8
peninah-njeri <peninahgathuru@gmail.com>
2019-08-18 22:46:18 +0300

    added a month function on the js if condition

--- scripts.js
+++ scripts.js
@@ -12,3 +12,15 @@ function validateform() {
         alert("continue")
     }
 }
+
+if(isNaN(month.value) ||month.value <1 || month.value >12 ||month.value === null || month.value === ""){
+    alert("Invalid month format");
+}else{
+    alert("continue")
+}
+
+if(isNaN(year.value) ||year.value <1 || year.value >1970 ||year.value === null || year.value === ""){
+    alert("Invalid year format");
+}else{
+    alert("continue")
+}
