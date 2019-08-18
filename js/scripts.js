9f7e14b2a34684843a94315c2973c96769bd5ec2
peninah-njeri <peninahgathuru@gmail.com>
2019-08-18 21:41:53 +0300

    modified scripts file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function validateform() {
+    const date = document.getElementById("date");
+    // const month = document.getElementById("month");
+    // const year = document.getElementById("year");
+    // const gen= document.getElementsByName("gender");
+    // const currentDate = new Date();
+    // const currentYear = currentDate.getFullYear();
+
+     if(isNaN(date.value) ||date.value <1 || date.value >31 ||date.value === null || date.value === ""){
+        alert("Invalid date format");
+    }else{
+        alert("continue");
+    }
+}
