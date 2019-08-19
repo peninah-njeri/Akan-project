f9c25895585979c28597a963ad1e9a6e4c9ffdc1
peninah-njeri <peninahgathuru@gmail.com>
2019-08-19 17:25:18 +0300

    modified README.md,css/styles.css,added a new image in the image folder, modified js/scripts.js, deleted scripts.js,styles.css

---
+++ README.md
@@ -0,0 +1,32 @@
+# Akan Names Project
+
+## Description
+  The project generates the Akan names subject to the details entered by the user as per the requirements.
+## Authors
+ This project is solely done by Pesh Njeri
+
+## BDD
+*  Give the user a form for date month, year and century input.
+* Provide an easy way that allows the user to input their gender. Options provided are either male or female
+* Ensure that the date value entered is a valid date.
+* Find the day of the week from the date input and combine that with the gender to output an Akan name that  corresponds to that day of the week.
+
+
+## Technologies Used
+* HTML
+* Code editor
+* Javascript
+* Bootstrap
+* Css
+
+## Link to live website
+ https://peninah-njeri.github.io/Akan-project/
+
+## Support and contact details
+Any queries?, concerns?....please feel free to contact me. 
+<ninahmjeri17@gmail.com>
+
+## License Info
+MIT License
+
+Copyright (c) 2019 Pesh
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,86 @@
-function validateform() {
-    const date = document.getElementById("date");
-    // const month = document.getElementById("month");
-    // const year = document.getElementById("year");
-    // const gen= document.getElementsByName("gender");
-    // const currentDate = new Date();
-    // const currentYear = currentDate.getFullYear();
+function akanNames() {
+    var currentYear = new Date ();
+    var maxYear = currentYear.getFullYear();
+    var day = document.getElementById("day");
+    var month = document.getElementById("month");
+    var year = document.getElementById("year");
 
-     if(isNaN(date.value) ||date.value <1 || date.value >31 ||date.value === null || date.value === ""){
-        alert("Invalid date format");
-    }else{
-        alert("continue");
+
+    if (year > maxYear || year < 1919) {
+        alert("Invalid year");
+        document.getElementById("year").value = document.getElementById("year").value.replace(characters, "");
+            evt.preventDefault()
+    }else if(day.value >31 || day.value <=0 || day.value === ""){
+        alert("Invalid date");
+        evt.preventDefault()
+    }else{ 
+        if (month.value > 12 || month.value === "" || month.value <= 0){
+        alert("Invalid month");
+        evt.preventDefault()
     }
+ }
 }
+      
+
+function calc(){
+    var cc = parseInt(document.getElementById("year").value.slice(0,2));
+    var yy = parseInt(document.getElementById("year").value.slice(2,4));
+    var mm = parseInt(document.getElementById("month").value);
+    var dd = parseInt(document.getElementById("day").value);
+    var gender=document.querySelector('input[name=gender]:checked').value;
+    alert(gender);   
+    var day = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7 ;
+    var d = parseInt(day.toFixed());
+  
+    
+    if (gender==='male'){
+      if (d === 0){
+        alert(" You were born on Sunday and your Akan name is Kwasi");
+      }
+      else if (d === 1){
+        alert(" You were born on Monday and your Akan name is Kwadwo");
+      }
+      else if (d === 2){
+        alert("You were born on Tuesday and your Akan name is Kwabena");
+      }
+      else if (d === 3){
+        alert("You were born on Wednesday and your Akan name is Kwaku");
+      }
+      else if (d === 4){
+        alert("You were born on Thursday and your Akan name is Yaw");
+      }
+      else if (d === 5){
+        alert("You were born on Friday and your Akan name is Kofi");
+      }
+      else {
+        alert("You were born on Saturday and your Akan name is Kwame");
+      }
+    }
+    if (gender=='female'){
+      if (d === 0){
+        alert("You were born on Sunday and your Akan name is Akosua");
+      }
+      else if (d === 1){
+        alert("You were born on Monday and your Akan name is Adwoa");
+      }
+      else if (d === 2){
+        alert("You were born on Tuesday and your Akan name is Abenaa");
+      }
+      else if (d === 3){
+        alert("You were born on Wednesday and your Akan name is Aku");
+      }
+      else if (d === 4){
+        alert("You were born on Thursday and your Akan name is Yaa");
+      }
+      else if (d === 5){
+        alert("You were born on Friday and your Akan name is Afua");
+      }
+      else {
+        alert("You were born on Saturday and your Akan name is Ama");
+      }
+    }
+  }
+  
+  
+
+
